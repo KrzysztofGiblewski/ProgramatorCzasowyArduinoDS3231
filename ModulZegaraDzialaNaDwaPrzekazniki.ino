4c4c593e44de49cc7d72b610c82e8eb51887196d
Krzysztof Giblewski <krzysztofgiblewski@gmail.com>
2019-06-13 11:03:28 +0200

    zdefiniowane dwa dodatkowe przekazniki

--- ModulZegaraDzialaNaDwaPrzekazniki.ino
+++ ModulZegaraDzialaNaDwaPrzekazniki.ino
@@ -2,9 +2,11 @@
 #include <Servo.h>
 #include <Wire.h>
 #include <DS3231.h>
+
 #define PRZEKAZNIK 13
 #define PRZEKAZNIK 10
-
+#define PRZEKAZNIK 8
+#define PRZEKAZNIK 9
 
  int minuty ;
  int godziny ;
@@ -16,7 +18,7 @@
  int wlonczz =7; 
  int wylonczz =19;
  
- int wlonczKarm1 =9;
+ int wlonczKarm1 =10;
  int wlonczKarm2 =15;
  
  int serwopozycja =0;
@@ -27,70 +29,71 @@
  Servo myservo;  // create servo object to control a servo
   DS3231 Clock;
   RTClib RTC; //modul zegara
-  
+  DateTime  now= RTC.now();
 
-
-
+//RS do pinu 12 (en to E) E do 11 itd
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-
-
- 
 void setup(){
   lcd.begin(16, 2);
   Serial.begin(9600);
+  // tu podpinam przekazniki do oświetlenia 
   pinMode(13, OUTPUT);
-  pinMode(10, OUTPUT);
   digitalWrite(13,HIGH);
+  
+  pinMode(10, OUTPUT);
   digitalWrite(10,HIGH);
+  
+  pinMode(8, OUTPUT);
+  digitalWrite(8,HIGH);
+  
+  pinMode(9, OUTPUT);
+  digitalWrite(9,HIGH);
+  
   pinMode(LED_BUILTIN, OUTPUT);               //wbudowana dioda led na plytce
 
    myservo.attach(9);  //  pin 9 sygnal dla serwa servo 
    myservo.write(0);    //  ustawiam serwo na kąt 0 stopni          
 
-
-
-/////////////////////// to po kolei odkomentować żeby ustawić zegar a potem zakomentować prymitywne ale dziala
+////////////*********************************************************************************/////////// 
+// ******* to po kolei wybraną linie odkomentować żeby ustawić zegar a potem zakomentować,  prymitywne ale dziala ;)
+//ROK
 //   Clock.setYear(19);
+//MIESIAC
 //   Clock.setMonth(6);
+//DZIEN
 //   Clock.setDate(11);
+//DZIEN TYGODNIA NP 1 TO PONIEDZIAŁEK
 //   Clock.setDoW(2);
+//GODZINA
 //   Clock.setHour(12);
+//MINUTY
 //   Clock.setMinute(54);
+//SEKUNDY
 //   Clock.setSecond(15);
-////////////////////////////////////////////////////////////////////////////
-
+//////////////*********************************************************************************////////////
 }
  
 void loop(){
- 
-    
-    DateTime  now= RTC.now();
-
-  
-
+    now= RTC.now();
     godziny=now.hour();
     minuty=now.minute();
     sekundy=now.second();
 
  delay(500);
  {
-  
+    //wyświetla na monitorze portu szeregowego
     Serial.print("godzina: ");
     Serial.print(godziny);
     Serial.print(" : ");  
     Serial.print(minuty);
     Serial.print(" : ");
     Serial.println(sekundy);
-
       
        digitalWrite(LED_BUILTIN, HIGH); //dodałem migającą diodę wbudowanaą na płytce
        delay(500);
        digitalWrite(LED_BUILTIN,LOW);
-      
-       
-
 
 //tu pulapki czasowe dla aktywacji przekaznikow
    if (godziny==wloncz){
@@ -125,7 +128,6 @@ void loop(){
      delay(1000);
          }
 
-
     lcd.setCursor(0,0);
     if (godziny <10)
     lcd.print(0);
@@ -139,7 +141,6 @@ void loop(){
     lcd.print(0);
     lcd.print(sekundy);
     lcd.print(" Karmie ");
-
     lcd.setCursor(0,1);
 //     lcd.print(" ");
 //     lcd.print(wloncz);
@@ -156,8 +157,6 @@ void loop(){
      lcd.print(" *");
      lcd.print(ilosc);
      lcd.print("syp");
-     
-
   
  }
  
